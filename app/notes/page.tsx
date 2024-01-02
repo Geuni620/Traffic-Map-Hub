6e98e001350a04d33615cc82291e6dea7305686d
Geuni <dlrmsgnl620@naver.com>
2024-01-02 22:54:14 +0900

    init supabase next.js

---
+++ app/notes/page.tsx
@@ -0,0 +1,30 @@
+"use client";
+
+import {createClient} from "@/utils/supabase/client";
+import {useEffect, useState} from "react";
+
+export default function Page() {
+  const [notes, setNotes] = useState<any[] | null>(null);
+  const supabase = createClient();
+
+  useEffect(() => {
+    const getData = async () => {
+      const {data} = await supabase.from("notes").select();
+      setNotes(data);
+    };
+    getData();
+  }, []);
+
+  return <pre>{JSON.stringify(notes, null, 2)}</pre>;
+}
+
+// import { createClient } from '@/utils/supabase/server'
+// import { cookies } from 'next/headers'
+
+// export default async function Page() {
+//   const cookieStore = cookies()
+//   const supabase = createClient(cookieStore)
+//   const { data: notes } = await supabase.from('notes').select()
+
+//   return <pre>{JSON.stringify(notes, null, 2)}</pre>
+// }
