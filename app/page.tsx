6dafc5a83ad6b26b584531157f9e6c40eafda68f
Geuni <dlrmsgnl620@naver.com>
2024-01-04 17:24:28 +0900

    Feat: add seoul traffic position

--- app/page.tsx
+++ app/page.tsx
@@ -6,11 +6,15 @@ import { createClient } from '@/utils/supabase/server';
 export default async function Index() {
   const cookieStore = cookies();
   const supabase = createClient(cookieStore);
-  const { data: position } = await supabase.from('position').select('*');
+  const { data: highway } = await supabase.from('highway-position').select('*');
+  const { data: seoul } = await supabase
+    .from('seoul-traffic-position')
+    .select('*');
+  const combinedData = [...highway, ...seoul];
 
   return (
     <div className="flex w-full flex-1 flex-col items-center gap-20">
-      <MapContainer data={position} />
+      <MapContainer data={combinedData} />
     </div>
   );
 }
