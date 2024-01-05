268608131ea5294a5c17284335da32c7b01a62e3
Geuni <dlrmsgnl620@naver.com>
2024-01-05 18:30:56 +0900

    WIP: supabase typescript

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,21 @@
 import { MapContainer } from 'lib/kakao-map';
 import { cookies } from 'next/headers';
+import { Tables } from 'types/supabase';
 
 import { createClient } from '@/utils/supabase/server';
+
+// 'highway-position' 테이블의 행 타입
+type HighwayPositionRow = Tables<'highway-position'>;
+
+// 'seoul-traffic-position' 테이블의 행 타입
+type SeoulTrafficPositionRow = Tables<'seoul-traffic-position'>;
+
+// MapContainer 컴포넌트의 props 타입
+export type MapContainerProps = {
+  data:
+    | (HighwayPositionRow & { source: 'highway' })[]
+    | (SeoulTrafficPositionRow & { source: 'seoul' })[];
+};
 
 export default async function Index() {
   const cookieStore = cookies();
--- lib/kakao-map/index.tsx
+++ lib/kakao-map/index.tsx
@@ -1,13 +1,10 @@
 'use client';
 
+import { type MapContainerProps } from 'app/page';
 import { LOCATION } from 'constant/geo-location';
 import Script from 'next/script';
 import { useState } from 'react';
 import { Map, MapMarker } from 'react-kakao-maps-sdk';
-
-type MapContainerProps = {
-  data: any;
-};
 
 export const MapContainer: React.FC<MapContainerProps> = ({ data }) => {
   const [zoomLevel, setZoomLevel] = useState(3);
@@ -26,24 +23,29 @@ export const MapContainer: React.FC<MapContainerProps> = ({ data }) => {
         onZoomChanged={(map) => setZoomLevel(map.getLevel())}
       >
         {zoomLevel < 10 &&
-          data.map((item, index) => (
-            <MapMarker
-              key={index}
-              position={{ lat: item.XCODE, lng: item.YCODE }}
-              image={{
-                src:
-                  item.source === 'highway'
-                    ? '/images/h-maker.png'
-                    : '/images/s-maker.png',
-                size: {
-                  width: 25,
-                  height: 25,
-                },
-              }}
-            >
-              {/* 마커 클릭 시 표시될 내용 */}
-            </MapMarker>
-          ))}
+          data.map((item, index) => {
+            return (
+              <MapMarker
+                key={index}
+                position={{
+                  lat: item.XCODE as number,
+                  lng: item.YCODE as number,
+                }}
+                image={{
+                  src:
+                    item.source === 'highway'
+                      ? '/images/h-maker.png'
+                      : '/images/s-maker.png',
+                  size: {
+                    width: 25,
+                    height: 25,
+                  },
+                }}
+              >
+                {/* 마커 클릭 시 표시될 내용 */}
+              </MapMarker>
+            );
+          })}
       </Map>
     </>
   );
