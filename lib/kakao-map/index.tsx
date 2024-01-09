09cdb8166c5f57da0f3caa76bd991a50a88625c6
Geuni <dlrmsgnl620@naver.com>
2024-01-09 14:22:19 +0900

    Chore: remove unnecessary file & run eslint

--- lib/kakao-map/index.tsx
+++ lib/kakao-map/index.tsx
@@ -1,10 +1,9 @@
 'use client';
 
-import { Badge } from 'app/components/ui/badge';
 import { type MapContainerProps } from 'app/page';
 import { LOCATION } from 'constant/geo-location';
 import Script from 'next/script';
-import { useState } from 'react';
+import { Fragment } from 'react';
 import {
   CustomOverlayMap,
   Map,
@@ -13,10 +12,39 @@ import {
 } from 'react-kakao-maps-sdk';
 import { formatNumberWithCommas } from 'utils/formatNumberWithCommas';
 
-export const MapContainer: React.FC<MapContainerProps> = ({ data }) => {
-  const [zoomLevel, setZoomLevel] = useState<number>(10);
+import { Badge } from '@/components/ui/badge';
 
-  console.log(zoomLevel);
+// interface DataItem {
+//   XCODE: number | null;
+//   YCODE: number | null;
+//   source: string;
+//   '2022_aadt': number;
+// }
+
+export const MapContainer: React.FC<MapContainerProps> = ({ data }) => {
+  // const [zoomLevel, setZoomLevel] = useState<number>(10);
+  // const [visibleData, setVisibleData] = useState<DataItem[]>([]);
+
+  // const handleBoundsChange = (map: kakao.maps.Map) => {
+  //   const bounds: kakao.maps.LatLngBounds = map.getBounds();
+  //   const swLatLng: kakao.maps.LatLng = bounds.getSouthWest();
+  //   const neLatLng: kakao.maps.LatLng = bounds.getNorthEast();
+
+  //   const filteredData: DataItem[] = data.filter((item) => {
+  //     return (
+  //       item.XCODE &&
+  //       item.YCODE &&
+  //       item.XCODE >= swLatLng.getLat() &&
+  //       item.XCODE <= neLatLng.getLat() &&
+  //       item.YCODE >= swLatLng.getLng() &&
+  //       item.YCODE <= neLatLng.getLng()
+  //     );
+  //   });
+
+  //   setVisibleData(filteredData);
+  // };
+
+  // console.log('visible', visibleData);
 
   return (
     <>
@@ -29,15 +57,15 @@ export const MapContainer: React.FC<MapContainerProps> = ({ data }) => {
         center={{ lat: LOCATION.LATITUDE, lng: LOCATION.LONGITUDE }}
         style={{ width: '100%', height: '100vh' }}
         level={12}
-        onZoomChanged={(map) => setZoomLevel(map.getLevel())}
+        // onBoundsChanged={(map) => handleBoundsChange(map)}
+        // onZoomChanged={(map) => setZoomLevel(map.getLevel())}
       >
-        <MarkerClusterer averageCenter={false} minLevel={8}>
+        <MarkerClusterer gridSize={300} averageCenter={false} minLevel={8}>
           {data.map((item, index) => {
             if (item.XCODE && item.YCODE) {
               return (
-                <>
+                <Fragment key={index}>
                   <MapMarker
-                    key={index}
                     position={{
                       lat: item.XCODE as number,
                       lng: item.YCODE as number,
@@ -65,7 +93,7 @@ export const MapContainer: React.FC<MapContainerProps> = ({ data }) => {
                   >
                     <Badge>{formatNumberWithCommas(item['2022_aadt'])}</Badge>
                   </CustomOverlayMap>
-                </>
+                </Fragment>
               );
             }
 
