1e2c295311349e8e2b6b555f875d0a6e255a7827
Geuni <dlrmsgnl620@naver.com>
2024-01-10 03:26:08 +0900

    WIP: performance

--- app/page.tsx
+++ app/page.tsx
@@ -31,19 +31,35 @@ export default async function Index() {
   const cookieStore = cookies();
   const supabase = createClient(cookieStore);
 
-  const { data: highwayData } = await supabase
-    .from('highway-position')
-    .select('*')
-    .returns<HighwayPositionRow[]>();
+  // const { data: highwayData } = await supabase
+  //   .from('highway-position')
+  //   .select('*')
+  //   .returns<HighwayPositionRow[]>();
 
-  const { data: seoulData } = await supabase
-    .from('seoul-traffic-position')
-    .select('*')
-    .returns<SeoulTrafficPositionRow[]>();
+  // const { data: seoulData } = await supabase
+  //   .from('seoul-traffic-position')
+  //   .select('*')
+  //   .returns<SeoulTrafficPositionRow[]>();
 
-  const { data: incheonData } = await supabase
-    .from('incheon-traffic-position')
-    .select('*');
+  // const { data: incheonData } = await supabase
+  //   .from('incheon-traffic-position')
+  //   .select('*');
+  console.time();
+  const [highwayData, seoulData, incheonData] = await Promise.all([
+    supabase
+      .from('highway-position')
+      .select('*')
+      .then((res) => res.data),
+    supabase
+      .from('seoul-traffic-position')
+      .select('*')
+      .then((res) => res.data),
+    supabase
+      .from('incheon-traffic-position')
+      .select('*')
+      .then((res) => res.data),
+  ]);
+  console.timeEnd();
 
   const highway = highwayData?.map((item) => ({
     ...item,
