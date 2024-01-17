c67fd536aa3e5445c1ef49700cd2805508e67539
Lee Keun Hwee <56650238+Geuni620@users.noreply.github.com>
2024-01-17 17:54:50 +0900

    [Feat] lazy loading, loading indictor (#1)
    * Feat: map loading indictor
    
    * Refactor: separate loading components

--- lib/kakao-map/index.tsx
+++ lib/kakao-map/index.tsx
@@ -1,22 +1,15 @@
 'use client';
 
 import { type MapContainerProps } from 'app/page';
+import { LegendViewWrapper } from 'components/common/legend-wrapper';
+import { LoadingSpinner } from 'components/common/LoadingSpinner';
 import { RadioButtonHandler } from 'components/common/radio-wrapper';
-import { Badge } from 'components/ui/badge';
 import { LOCATION } from 'constant/geo-location';
-import { Fragment, useState } from 'react';
-import {
-  CustomOverlayMap,
-  Map,
-  MapMarker,
-  MarkerClusterer,
-} from 'react-kakao-maps-sdk';
-import { formatNumberWithCommas } from 'utils/formatNumberWithCommas';
-import { formatType } from 'utils/formatType';
-import { getMarkerImage } from 'utils/getMarkerImage';
-import { getTrafficColor } from 'utils/getTrafficColor';
+import { lazy, Suspense } from 'react';
+import { useState } from 'react';
+import { Map, MarkerClusterer } from 'react-kakao-maps-sdk';
 
-import { LegendViewWrapper } from '@/components/common/legend-wrapper';
+const MapMarkerComp = lazy(() => import('components/map/map-marker'));
 
 export type CategoryFilter = 'all' | 'highway' | 'seoul' | 'incheon' | 'toll';
 export type RoadType = 'expressway' | 'national' | 'provincial' | 'local';
@@ -57,56 +50,9 @@ export const MapContainer: React.FC<MapContainerProps> = ({ data }) => {
               averageCenter={true}
               minLevel={8}
             >
-              {filteredData.map((item, index: number) => {
-                if (item.XCODE && item.YCODE) {
-                  const aadtKey =
-                    item.source === 'toll' ? '2021_aadt' : '2022_aadt';
-                  const badgeColor = getTrafficColor(item[aadtKey]);
-                  const markerImageSrc = getMarkerImage(
-                    item.source,
-                    item.road_type,
-                  );
-
-                  return (
-                    <Fragment key={index}>
-                      <MapMarker
-                        position={{
-                          lat: item.XCODE as number,
-                          lng: item.YCODE as number,
-                        }}
-                        image={{
-                          src: markerImageSrc,
-                          size: {
-                            width: 25,
-                            height: 25,
-                          },
-                        }}
-                      />
-                      <CustomOverlayMap
-                        position={{
-                          lat: item.XCODE as number,
-                          lng: item.YCODE as number,
-                        }}
-                        xAnchor={0.5}
-                        yAnchor={2.0}
-                      >
-                        <Badge
-                          className="p-0"
-                          style={{
-                            backgroundColor: badgeColor,
-                          }}
-                        >
-                          <p className="badge-label">
-                            {formatType(item.type).label}{' '}
-                            {formatNumberWithCommas(item[aadtKey])}
-                          </p>
-                        </Badge>
-                      </CustomOverlayMap>
-                    </Fragment>
-                  );
-                }
-                return null;
-              })}
+              <Suspense fallback={<LoadingSpinner />}>
+                <MapMarkerComp filteredData={filteredData} />
+              </Suspense>
             </MarkerClusterer>
           </Map>
         </RadioButtonHandler>
