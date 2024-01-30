db0513ef410ab3fd9e64f7504c7bf14b8fb1e3df
Geuni <dlrmsgnl620@naver.com>
2024-01-30 13:16:24 +0900

    Feat: 줌 레벨이 13보다 작을 경우(축소했을 떄), 마커를 렌더링하지 않도록 변경

--- components/map/marker-container.tsx
+++ components/map/marker-container.tsx
@@ -1,6 +1,9 @@
 import { type TrafficHub } from 'app/page';
+import { useExternalValue } from 'external-state';
 import { useTrafficGetQuery } from 'hooks/useTrafficGetQuery';
 import dynamic from 'next/dynamic';
+import { useEffect, useState } from 'react';
+import { getGoogleMapStore } from 'store/googleMapStore';
 
 const TrafficMapMarker = dynamic(() => import('@/components/map/marker'), {
   ssr: false,
@@ -13,7 +16,36 @@ type MarkerContainerProps = {
 export const MarkerContainer: React.FC<MarkerContainerProps> = ({
   selectedCategory,
 }) => {
-  const { traffic } = useTrafficGetQuery({ categoryFilter: selectedCategory });
+  const mapStore = getGoogleMapStore?.();
+  if (!mapStore) return;
+
+  const googleMaps = useExternalValue(mapStore);
+  const [currentZoomLevel, setCurrentZoomLevel] = useState(
+    googleMaps?.getZoom(),
+  );
+
+  useEffect(() => {
+    if (!googleMaps) return;
+
+    const onZoomChanged = () => {
+      setCurrentZoomLevel(googleMaps.getZoom());
+    };
+
+    const listener = googleMaps.addListener('zoom_changed', onZoomChanged);
+
+    return () => {
+      google.maps.event.removeListener(listener);
+    };
+  }, [googleMaps]);
+
+  const { traffic } = useTrafficGetQuery({
+    categoryFilter: selectedCategory,
+    currentZoomLevel,
+  });
+
+  if (currentZoomLevel && currentZoomLevel <= 13) {
+    return null;
+  }
 
   if (traffic.data === undefined) {
     return null;
--- hooks/useTrafficGetQuery.ts
+++ hooks/useTrafficGetQuery.ts
@@ -3,6 +3,34 @@ import { trafficManagerKeys } from 'lib/query/queryFactory';
 import { getGoogleMapStore } from 'store/googleMapStore';
 import { getDisplayPosition } from 'utils/getDisplayPosition';
 
+const fetchCluster = async ({ categoryFilter }: UseTrafficGetQueryType) => {
+  const mapStore = getGoogleMapStore?.();
+  if (!mapStore) return;
+
+  const { latitudeDelta, longitudeDelta, longitude, latitude } =
+    getDisplayPosition(mapStore.getState());
+
+  const url = new URL(
+    `${process.env.NEXT_PUBLIC_API_URL}/api/traffic/clusters`,
+  );
+  url.searchParams.append('latitude', latitude.toString());
+  url.searchParams.append('longitude', longitude.toString());
+  url.searchParams.append('latitudeDelta', latitudeDelta.toString());
+  url.searchParams.append('longitudeDelta', longitudeDelta.toString());
+  url.searchParams.append(
+    'category',
+    Array.from(categoryFilter || []).join(','),
+  );
+
+  const res = await fetch(url);
+
+  if (!res.ok) {
+    throw new Error('마커를 불러오는데 실패했어요..!');
+  }
+
+  return await res.json();
+};
+
 const fetchTraffic = async ({ categoryFilter }: UseTrafficGetQueryType) => {
   const mapStore = getGoogleMapStore?.();
   if (!mapStore) return;
@@ -31,18 +59,25 @@ const fetchTraffic = async ({ categoryFilter }: UseTrafficGetQueryType) => {
 
 type UseTrafficGetQueryType = {
   categoryFilter?: Set<string>;
+  currentZoomLevel?: number;
 };
 
 export const useTrafficGetQuery = ({
   categoryFilter,
+  currentZoomLevel,
 }: UseTrafficGetQueryType) => {
+  const queryFunction =
+    currentZoomLevel && currentZoomLevel >= 13 ? fetchTraffic : fetchCluster;
+
   const traffic = useQuery({
     queryKey: [
       ...trafficManagerKeys.traffic,
       'category',
       Array.from(categoryFilter || []).join(','),
+      'zoomLevel',
+      currentZoomLevel,
     ],
-    queryFn: () => fetchTraffic({ categoryFilter }),
+    queryFn: () => queryFunction({ categoryFilter }),
   });
 
   return {
