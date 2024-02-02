2372ad37f8c5889fb10af6237c0daebbbd17ce30
Geuni <dlrmsgnl620@naver.com>
2024-02-02 23:09:15 +0900

    Feat: add og images

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,15 +5,33 @@ import { LoadingSpinner } from 'components/common/loading-spinner';
 import { QueryContext } from 'components/common/query-context';
 import { GeistSans } from 'geist/font/sans';
 import { Suspense } from 'react';
-
-const defaultUrl = process.env.VERCEL_URL
-  ? `https://${process.env.VERCEL_URL}`
-  : 'http://localhost:3000';
+import CONFIG from 'site.config';
 
 export const metadata = {
-  metadataBase: new URL(defaultUrl),
-  title: 'Traffic Hub',
-  description: '전국에 있는 모든 교통량을 보여드릴게요.',
+  metadataBase: new URL(CONFIG.url),
+  title: CONFIG.title,
+  description: CONFIG.description,
+  icons: {
+    icon: {
+      url: '/favicon.png',
+      type: 'image/png',
+      sizes: '32x32',
+    },
+  },
+
+  openGraph: {
+    title: CONFIG.title,
+    description: CONFIG.description,
+    url: CONFIG.url,
+    siteName: CONFIG.title,
+    locale: 'ko-KR',
+    type: 'website',
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: CONFIG.title,
+    description: CONFIG.description,
+  },
 };
 
 export default function RootLayout({
